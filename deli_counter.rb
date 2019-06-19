b671b1679d8d81ffdd4a4fbe152de8fba93af95a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 21:30:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   else 
     current_line = "The line is currently:"
       array.each.with_index do |person, index|
-        current_line << "#{index} #{person}"
+        current_line << "#{index}. #{person}."
       end
       puts current_line
   end
