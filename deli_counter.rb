1b88a6e87b38a2985fc6f747ca1e93fbaf7b88f7
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 21:27:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 
 def line(array)
-  array = []
+   array = []
   if array.count == 0
     puts "The line is currently empty."
   else 
     current_line = "The line is currently:"
-    array.each_with_index do |person, index|
-      puts "#{index} #{person}"
+      array.each_with_index do |person, index|
+        puts "#{index} #{person}"
+      end
   end
 end 
 
